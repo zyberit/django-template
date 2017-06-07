1f12e1cb379d5b8bf11f9993c740f2a8757d9640
Håkan Persson <hakan@violaberg.nu>
2017-06-07 11:35:52 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kbgh^f#c+7t%a412%wn*x6$-p_lk1t+q9*95cgp&@=2rtmv-(x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
